845b072e0ecf0830572aef194102f1561133c3b5
SashaMac <qwerty654951@mail.ru>
2024-03-01 22:24:59 +0300

    1.03.2024

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import cv2
 import math
 import numpy as np
 
+#статистика кол ва камер 
+#госты 
+#важность ии  в современном мире 
+
+
+
+
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
     linear_valu = (1.0 - face_distance) / (range * 2.0)
